00ecbeef1fcb64e6063a08307611b96f69e24e04
childe <heychilde@gmail.com>
2015-07-10 01:20:53 +0800

    add login/logout

--- esproxy/urls.py
+++ esproxy/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
+from django_cas.views import login,logout
 
 urlpatterns = patterns(
     '',
     url(r'^elasticsearch/', 'esproxy.views.elasticsearch'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login.html/$', csrf_exempt(login)),
+    url(r'^logout.html/$',logout),
 )
