b01e7e8f59e19d1429d1b8971d652364e6e3f5c3
childe <heychilde@gmail.com>
2015-07-15 20:03:34 +0800

    add django-user header

--- esproxy/views.py
+++ esproxy/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from settings import ELASTICSEARCH_PROXY, ELASTICSEARCH_REAL,KIBANA_DIR
+from settings import ELASTICSEARCH_PROXY, ELASTICSEARCH_REAL, KIBANA_DIR
 
 
 def login_or_404(func):
@@ -18,21 +18,22 @@ def login_or_404(func):
     return inner
 
 
-#@login_or_404
+@login_or_404
 @csrf_exempt
 def elasticsearch(request):
     fullpath = request.get_full_path()
     fullpath = fullpath[len(ELASTICSEARCH_PROXY):]
     response = HttpResponse()
     response['X-Accel-Redirect'] = ELASTICSEARCH_REAL + '/' + fullpath
+    response['Django-User'] = request.user.username
     return response
 
 
 def home(request):
     return HttpResponseRedirect("index.html")
 
+
 @login_required
 def index(request):
-    html = open(os.path.join(KIBANA_DIR,"index.html")).read()
+    html = open(os.path.join(KIBANA_DIR, "index.html")).read()
     return HttpResponse(html)
-
